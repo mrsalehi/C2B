d68dc438b23f9210c405394714824baac67ab79a
mrsalehi <mrezasal1@gmail.com>
2021-05-20 22:29:03 -0400

    More vectorized multiplexing

--- simulation.py
+++ simulation.py
@@ -8,6 +8,41 @@ from utils import load_video
 S = 5
 
 
+def multiplex_v2(subframes: torch.Tensor, W: torch.Tensor, nbhds: List[List[tuple]]):
+    """
+    subframes: intensities of subframes of a frame as a numpy array with shape (S, W, H)
+    W: numpy array with shape (neighborhood_size, S)
+    nbhds: neighborhoods 
+    """
+    height, width = subframes.shape[1], subframes.shape[2]
+    c2b_frame_bucket0 = torch.zeros(height, width)
+    c2b_frame_bucket1 = torch.zeros(height, width)
+
+    # for nbhd in nbhds:
+    nbhds_rows = [[el[0] for el in nbhd] for nbhd in nbhds]
+    nbhds_cols = [[el[1] for el in nbhd] for nbhd in nbhds]
+
+    nbhd_size = len(nbhds_rows[0])
+    n_nbhds = len(nbhds_rows)
+
+    nbhd_subframes = [subframes[:, rows, cols] for rows, cols in zip(nbhds_rows, nbhds_cols)]  # list of tensors with shape (S, nbhd_size)
+    nbhd_subframes = torch.stack(nbhd_subframes, dim=0)  # shape: (n_nbhds, S, neighborhood_size)
+
+    nbhd_subframes_samples = torch.gather(nbhd_subframes, 2, torch.randint(nbhd_size, size=(n_nbhds, S, 1)))  # shape: (n_nbhds, S, 1)
+    c2b_nbhds_bucket0 = torch.matmul(nbhd_subframes_samples.permute(0, 2, 1), W.T).squeeze()  # shape: (nbhd_size,)
+    c2b_nbhds_bucket1 = torch.matmul(nbhd_subframes_samples.permute(0, 2, 1), 1 - W.T).squeeze()  # shape: (nbhd_size,)
+
+    
+    for i, (row, col) in enumerate(zip(nbhds_rows, nbhds_cols)):
+        c2b_frame_bucket0[row, col] = c2b_nbhds_bucket0[i]
+        c2b_frame_bucket1[row, col] = c2b_nbhds_bucket1[i]
+
+    #c2b_frame_bucket0[nbhd_row, nbhd_col] = c2b_nbhd_bucket0
+    #c2b_frame_bucket1[nbhd_row, nbhd_col] = c2b_nbhd_bucket1
+
+    return c2b_frame_bucket0, c2b_frame_bucket1
+
+
 def multiplex(subframes: torch.Tensor, W: torch.Tensor, nbhds: List[List[tuple]]):
     """
     subframes: intensities of subframes of a frame as a numpy array with shape (S, W, H)
@@ -47,7 +82,7 @@ height, width = subframes.shape[1], subframes.shape[2]
 nbhds = [[(2*i, 2*j), (2*i, 2*j+1), (2*i+1, 2*j), (2*i+1, 2*j+1)] \
     for i in range(int(height / 2)) for j in range(int(width / 2))]
 
-c2b_frame_bucket0, c2b_frame_bucket1 = multiplex(subframes, W, nbhds)
+c2b_frame_bucket0, c2b_frame_bucket1 = multiplex_v2(subframes, W, nbhds)
 
 
 # def get_bucket_measurements(input: np.ndarray):
